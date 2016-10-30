2884981bd69456c565121be157c49641b4476a01
Daniel Montoya <dsmontoyam@gmail.com>
2016-10-29 22:13:31 -0500

    Find transactions by user tel.

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"os"
+
+	"github.com/entropyx/fiduchain/controllers"
+	"github.com/gin-gonic/gin"
+	mgo "gopkg.in/mgo.v2"
+)
 
 func main() {
+	mongoPath := fmt.Sprintf("%s:%s/%s", os.Getenv("MONGO_HOST"), os.Getenv("MONGO_PORT"), os.Getenv("MONGO_DB"))
+	session, err := mgo.Dial(mongoPath)
+	defer session.Close()
+	if err != nil {
+		panic(err)
+	}
+	ctrl := controllers.New(session)
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/users/:tel/transactions", ctrl.GetUserTransactions)
 	r.Run(":8081") // listen and server on 0.0.0.0:8080
 }
